85a0c558fd89f20a50532723916d1b525260d8d7
mbieweng <mike@digitalaltitudes.com>
2019-02-18 13:51:45 -0600

    Fixes to merge FPU with override branches

--- LoopKitUI/CarbKit/DecimalTextFieldTableViewCell.swift
+++ LoopKitUI/CarbKit/DecimalTextFieldTableViewCell.swift
@@ -8,6 +8,43 @@
 
 import UIKit
 
+class DecimalTextFieldTableViewCell: TextFieldTableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        
+        return formatter
+    }()
+    
+    var number: NSNumber? {
+        get {
+            return numberFormatter.number(from: textField.text ?? "")
+        }
+        set {
+            if let value = newValue {
+                textField.text = numberFormatter.string(from: value)
+            } else {
+                textField.text = nil
+            }
+        }
+    }
+    
+    // MARK: - UITextFieldDelegate
+    
+    override func textFieldDidEndEditing(_ textField: UITextField) {
+        if let number = number {
+            textField.text = numberFormatter.string(from: number)
+        } else {
+            textField.text = nil
+        }
+        
+        super.textFieldDidEndEditing(textField)
+    }
+}
+
 class CarbDecimalTextFieldTableViewCell: TextFieldTableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
